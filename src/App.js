d7126a2d5d4ffce1598267a642281569b9a25c7c
k-imun <endrmftls@naver.com>
2025-01-21 12:48:18 +0900

    1_2 components 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import AboutView from './views/AboutView';
+import HomeView from './views/HomeView';
 
 const App = ()=>{
-  return <div>App</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' elment={<HomeView />} />
+        <Route path='/about' elment={<AboutView />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
+// BrowserRouter: 라우팅을 적용하기 위한 컴포넌트입니다. App 컴퍼넌트를 감싸서 라우팅(실행) 설정을 적용할 수 있는 명령어.
+// Routes: 라우터들을 정의하는 컴테이너입니다. 명령어들을 가지고 있다고 보시면 됩니다.
+// <Route path='/' element={<HomeView/>}: '/' 경로에 해당하는 <HomeView/> 컴퍼넌트를 랜더링하도록 설정.
 export default App;
---
+++ src/views/HomeView.jsx
@@ -0,0 +1,10 @@
+// rafce 엔터
+import React from 'react'
+
+const HomeView = () => {
+  return (
+    <div>HomeView</div>
+  )
+}
+
+export default HomeView
